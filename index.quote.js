d2398524a7e4b8cb7022eab79a17103fac8b76c0
davidlescard <davidlescard@gmail.com>
2020-07-19 00:27:58 -0400

    quote working

--- index.quote.js
+++ index.quote.js
@@ -4,35 +4,39 @@ const request = require('request');
 let refreshFile = '../aut/tok.json';
 let responseFile = '../aut/response.json';
 let clientid = fse.readFileSync('../aut/client.id', 'utf8');
+let content_type = 'application/json';
+let quoteUrl = 'https://api.tdameritrade.com/v1/marketdata/chains';
 
 let token_file = fse.readFileSync(refreshFile, 'utf8');
 let token_JSON = JSON.parse(token_file);
-let access_code = `Bearer ${token_JSON.access_token}`;
-console.log(access_code);
+let access_code = 'Bearer ' + token_JSON.access_token;
+// console.log(access_code);
 
+/* refresh token if greater than 30 min */
 let elapsed = Date.now() - token_JSON.timestamp;
-console.log(elapsed);
-console.log(token_JSON.timestamp);
-if (elapsed / 1000 > 30 ) {
-    console.log('fetching new access token');
+if (elapsed / 1000 > 29 * 60 ) {
+    // console.log(elapsed);
+    // console.log(token_JSON.timestamp);
+    // console.log('fetching new access token');
     require('./index.access');
-    console.log('hopefully got new access token');
+    // console.log('hopefully got new access token');
 }
 
-process.exit(0);
 let ticker = 'T';
 
 let options = {
-    url: 'https://api.tdameritrade.com/v1/marketdata/chains',
-    method: 'GET',
-    headers: { 'Authorization': access_code },
-    form: {
-        'symbol': ticker,
-        'apikey': clientid,
+    url: quoteUrl,
+    headers: { 
+        'Content-Type': content_type,
+        'Authorization':  access_code
+    },
+    qs: {
+        symbol: ticker,
+        apikey: clientid
     }
 }
 
-request(options, function(error, response, body) {
+request.get(options, function(error, response, body) {
     httpStatusCode = (response === undefined) ? 0 : response.statusCode;
 
     if (response.statusCode == 200) {
@@ -43,6 +47,5 @@ request(options, function(error, response, body) {
         console.log(httpStatusCode);
         console.log(body);
     }
-
 
 });
