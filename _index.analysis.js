bb45052c7142875eede01a6cd5d0fec561e45bc9
davidlescard <davidlescard@gmail.com>
2021-04-04 20:44:51 -0400

    added history - doesnt work

--- _index.analysis.js
+++ _index.analysis.js
@@ -5,6 +5,7 @@ const constants = require('./lib/td/constants.json');
 const LoadedAuth = require('./lib/td/auth/LoadedAuth');
 const Option = require('./lib/td/quote/Option');
 const Instrument = require('./lib/td/quote/Instrument');
+const Price = require('./lib/td/quote/Price');
 const Orders = require('./lib/td/accounts/Orders');
 const Accounts = require('./lib/td/accounts/Accounts');
 const Quote = require('./lib/td/quote/Quote');
@@ -13,8 +14,15 @@ const StrategyOrder = require('./lib/td/strategy/Order');
 
 async function doit () {
 
-    let tickers = ['T', 'DAL', 'JETS', 'CCL', 'AAL', 'SAVE', 'UBER', 'BAC'];
+    let tickers = ['XLC', 'XLY', 'XLE', 'XLF', 'XLV', 'XLI', 'XLP', 'XLB', 'XLRE', 'XLK', 'XLU', 'SPY', ] ;
+    // let tickers = ['GOOG', 'NFLX', 'TSLA', 'SPY', 'QQQ', 'FB', 'MSFT'] ;
+    // let tickers = ['SPX', 'GOOG', 'RUT', 'NFLX', 'TSLA', 'SPY', 'QQQ', 'FB', 'MSFT'] 
+    // let tickers = ['T', 'DAL', 'JETS', 'CCL', 'AAL', 'SAVE', 'UBER', 'BAC', 
+    //     'SLV', 'KODK', 'TSLA', 'GLD', 'SLV', 'NFLX', 'QQQ', 'AAPL', 'GOOG'];
     let ticker = 'T';
+    // let ticker = 'SPX';
+    // let ticker = 'SLV';
+    // let ticker = 'NFLX';
     // let ticker = 'TSLA';
     // let ticker = 'KODK';
     let quotes = {};
@@ -23,33 +31,81 @@ async function doit () {
     let targetShortDelta = 0.16;
     let targetLongDelta = 0.05;
 
-    let targetProfit = 0.50;
-    let stopLoss = 2.0;
-    let stopLimitMultiplier = 1.03;
-    let stopLimit = stopLoss * stopLimitMultiplier;
+    let targetProfitRatio = 0.50;
+    let stopLossRatio = 2.0;
+    let stopTriggerHeadoffMultiplier = 0.99; //We're BUY_TO_CLOSE on a short option so we want to set a trigger just before our stop 
+    // let stopLimit = stopLossRatio * stopTriggerHeadoffMultiplier;
 
     let tickerIV = []
     let tickerIVMap = {};
 
 
-    let result = await StrategyOrder.createCondor(ticker, 
-        targetDTE, targetShortDelta, targetLongDelta,
-        targetProfit, stopLoss, stopLimitMultiplier);
+    /**
+     * create condor: trigger => OCO
+     */
+    // let result = await StrategyOrder.createCondor(ticker, 1,
+    //     targetDTE, targetShortDelta, targetLongDelta,
+    //     targetProfitRatio, stopLossRatio, stopTriggerHeadoffMultiplier);
         
-    process.exit(0)
+    // process.exit(0)
+    
+
+    /**
+     * create condor: trigger => limit
+     */
+    // let result = await StrategyOrder.createCondor(ticker, 1,
+    //     targetDTE, targetShortDelta, targetLongDelta,
+    //     targetProfitRatio);
+        
+    // process.exit(0)
     
     
     /**
      * form tickerIVMap
      */
-    for (ticker of tickers) {
-        let chain = (await Option.getOptionChain(ticker, {})).body;
-        let implV = Volatility.impliedVol(chain);
-        tickerIVMap[ticker] = {iv: undefined};
-        tickerIVMap[ticker].iv = implV; 
-    }
-    console.log(tickerIVMap);
+    // for (ticker of tickers) {
+    //     let chain = (await Option.getOptionChain(ticker, {})).body;
+    //     let implV = Volatility.impliedVol(chain);
+    //     tickerIVMap[ticker] = {iv: undefined};
+    //     tickerIVMap[ticker].iv = implV; 
+    // }
+    // console.log(tickerIVMap);
     
+    // process.exit(0)
+    
+    /**
+     * get one IV
+     */
+    // let chain = (await Option.getOptionChain(ticker, {})).body;
+    // let implV = Volatility.impliedVol(chain, 90);
+    // console.log(implV);
+    
+    // process.exit(0)
+    
+    /**
+     * get Intrument
+     */
+    // let inst = (await Instrument.getInstrument(ticker)).body;
+    // console.log(inst);
+    
+    // process.exit(0)
+    
+    /**
+     * get Intrument
+     */
+    //Example: For a 2 day / 1 min chart, the values would be:
+    //period: 2
+    //periodType: day
+    //frequency: 1
+    //frequencyType: min
+    let periodType = 'day';
+    let period = '2';
+    let frequencyType = 'min';
+    let frequency = '1';
+    let hist = (await Price.priceHistory(ticker, periodType, period, frequencyType, frequency)).body;
+    console.log(hist);
+    
+    process.exit(0)
     
 
     ////////////////////////////////////////////////////////////////////////////////////////
